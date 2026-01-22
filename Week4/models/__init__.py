25bbc213718cad88b42a2bea234128ce68e01e88
Alvaritox11 <alvaro11.8.03@gmail.com>
2026-01-22 14:41:02 +0100

    Experimental and base models and configs added

--- Week4/models/__init__.py
+++ Week4/models/__init__.py
@@ -3,3 +3,4 @@ from .squeeze_net import SqueezeNet
 from .shufflenet_mini import ShuffleNetMini
 from .mobilenet import MobileNet
 from .teacher_wrapper import TeacherWrapperModel
+from .experimental_net import ExperimentalCNN
